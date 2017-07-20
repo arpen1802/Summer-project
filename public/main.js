e22842957f211dd766064f685286b7e3a3f0109b
arpen1802 <matinyan.2000@gmail.com>
2017-07-20 22:37:11 +0400

    first commit

---
+++ public/main.js
@@ -0,0 +1,43 @@
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+function uppercase (string) {
+  for(let i = 0; i <string.length; i++){
+      if (string[i].toLowerCase() != string[i].toUpperCase() && string[i] === string[i].toUpperCase() ) {
+        return true;
+        }
+    }
+    return false;
+  }
+
+
+function hasnumbers(string) {
+  return /\d/.test(string);
+}
+
+  function length(string){
+    if(string.length > 8 && string.length < 16){
+      return true;
+    }
+    else{
+      return false;
+    }
+
+  }
+
+$("#donebutton").on("click", function() {
+  if ($("#login").val() != "" && $("#password").val() != ""){
+        if(validateEmail($("#login").val()) && uppercase($("#password").val()) && hasnumbers($("#password").val()) && length($("#password").val())){
+          console.log("Great!")
+        }
+        else{
+         console.log('Something went wrong!!! Try again.');
+        }
+      }
+    })
+
+
+
+//&& uppercase($("#password").val()) && hasnumbers($("#password").val()) && length($("#password").val())
